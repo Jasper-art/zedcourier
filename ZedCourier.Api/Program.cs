ede8955c3f4bd074f4e00ea5c06f4b6c052407f9
Pear Jasper <pearjasper@gmai.com>
2026-04-10 01:17:28 +0200

    fix CORS for production

--- ZedCourier.Api/Program.cs
+++ ZedCourier.Api/Program.cs
@@ -24,7 +24,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins(
+                "http://localhost:5173",
+                "https://zedcourier.vercel.app"
+              )
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
